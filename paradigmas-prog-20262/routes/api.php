a9b0295376f190e61a1e008c0e4f2a7b3aee3e2c
Gabriel Oliveira <goliveira182826@gmail.com>
2026-08-17 23:22:05 +0000

    Configurações da api.php

--- paradigmas-prog-20262/routes/api.php
+++ paradigmas-prog-20262/routes/api.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Models\User;
+use Iluminate\Http\Request;
+use Iluminate\Support\Facades\Request as FacadesRequest;
 use Iluminate\Support\Facades\Route;
 
-Route::get('/health', function()){
+Route::get('/health', function(){
     return response()->json('Minha API tá on');
+});
+
+Route::post('/users', function(){
+    $data = FacadesRequest::validate([
+        'name' => 'required|string|max:50|min:3',
+        'email' => 'required|email',
+        'password' => 'required|min:4|max:20',
+    ]);
+
+    User::create([
+        'name' => $data['name'],
+        'email' => $data['email'],
+        'password' => $data['password'],
+    ]);
+
+    return response()->json('Usuario criado com sucesso!');
 });
